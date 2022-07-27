d9ffed9e4f620bc1af21a0136ef423279c98793f
Mark Gerken <mgerken86@gmail.com>
2022-07-27 13:39:16 -0600

    auth routes / components on app.js

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -6,11 +6,14 @@ import GearIndexPage from '../GearIndexPage/GearIndexPage';
 import GearDetailPage from '../GearDetailPage/GearDetailPage';
 import ReservationDetailPage from '../ReservationDetailPage/ReservationDetailPage';
 import ReservationsIndexPage from '../ReservationsIndexPage/ReservationsIndexPage';
+import PrivateRoute from "./utils/PrivateRoute";
+import { AuthProvider } from "./context/AuthContext";
 
 export default function App() {
 
     return (
       <div className="App">
+        <AuthProvider>
         <NavBar/>
         <Routes>
             <Route path="/" element={<HomePage />} />
@@ -18,7 +21,11 @@ export default function App() {
             <Route path="/rentals/:gear_item_id" element={<GearDetailPage />} />
             <Route path="/reservations" element={<ReservationsIndexPage />} />
             <Route path="/reservations/:reservation_id" element={<ReservationDetailPage/>} />
+            <PrivateRoute path="/protected" element={<ProtectedPage/>} exact />
+            <Route path="/login" element={<LoginPage/>}/>
+            <Route path="/register" element={<RegisterPage/>}/>
           </Routes>
+          </AuthProvider>
       </div>
     );
   }
