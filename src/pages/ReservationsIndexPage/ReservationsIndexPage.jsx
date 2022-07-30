cc31174386670083eb02c0fac5d8b2e8ddfb8ba1
Mark Gerken <mgerken86@gmail.com>
2022-07-29 21:37:17 -0600

    react-big-calendar on reservation index page

--- src/pages/ReservationsIndexPage/ReservationsIndexPage.jsx
+++ src/pages/ReservationsIndexPage/ReservationsIndexPage.jsx
@@ -3,6 +3,11 @@ import NewReservationForm from '../../components/NewReservationForm/NewReservati
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { Navigate, useNavigate } from "react-router-dom";
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment)
 
 
 export default function ReservationsIndexPage() {
@@ -10,13 +15,11 @@ export default function ReservationsIndexPage() {
     const navigate = useNavigate()
     const [showForm, setShowForm] = useState(false)
 
-    let data
-
 
     const getReservations = () => {
         axios.get('https://a-lodge-basecamp.herokuapp.com/reservations/')
             .then(res => {
-                data = res.data;
+                let data = res.data;
                 console.log(data)
                 setReservations(data);
             })
@@ -33,26 +36,35 @@ export default function ReservationsIndexPage() {
     return (
         <main>
             <h1>Reservations Index Page</h1>
+            <div id='calendarCont'>
+                <Calendar
+                    localizer={localizer}
+                    //   events={myEventsList}
+                    startAccessor="start"
+                    endAccessor="end"
+                    style={{ height: 500 }}
+                />
+            </div>
             <div className='container-left'>
-            <button onClick={()=>setShowForm(!showForm)}>New Reservation</button>
-            {showForm && 
-            <div>
-                <NewReservationForm />
-            </div>}
-            {reservations.map((reservation, index) => {
-                return <div key={index}>
-                    <p>Start Date: {reservation.start_date}</p>
-                    <p>End Date: {reservation.end_date}</p>
-                    {reservation.gear_item.map((gear, index) => {
-                        return <div key={index}>
-                            <h3>Gear Item:</h3>
-                            <p>{gear.name}</p>
-                            {/* <p>${gear.price}</p> */}
-                            <p>Quantity: {reservation.qty}</p>
-                        </div>
-                    })}
-                </div>
-            })}
+                <button onClick={() => setShowForm(!showForm)}>New Reservation</button>
+                {showForm &&
+                    <div>
+                        <NewReservationForm />
+                    </div>}
+                {reservations.map((reservation, index) => {
+                    return <div key={index}>
+                        <p>Start Date: {reservation.start_date}</p>
+                        <p>End Date: {reservation.end_date}</p>
+                        {reservation.gear_item.map((gear, index) => {
+                            return <div key={index}>
+                                <h3>Gear Item:</h3>
+                                <p>{gear.name}</p>
+                                {/* <p>${gear.price}</p> */}
+                                <p>Quantity: {reservation.qty}</p>
+                            </div>
+                        })}
+                    </div>
+                })}
             </div>
         </main>
     )
