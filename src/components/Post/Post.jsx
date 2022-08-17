161b77eb2c3a7c0077424db1349fdab310fdcd93
Mark Gerken <mgerken86@gmail.com>
2022-08-17 11:30:23 -0600

    minor post styling

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -5,8 +5,8 @@ import moment from 'moment'
 export default function Post({ post }) {
     console.log(post)
     return (
-        <div id='Post'>
-            <h1>{post.h1}</h1>
+        <div id='Post' className='ordersCont'>
+            <h1>{post.title}</h1>
             <h2>{post.this_user}</h2>
             <h3>{post.this_topic}</h3>
             <h3>{moment(post.created_at).format('MM/DD/YYYY')}</h3>
