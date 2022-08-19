531232a59300526cc95b38db06fc1f2791114f14
Mark Gerken <mgerken86@gmail.com>
2022-08-19 09:58:27 -0600

    post buttons all on bottom of post

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -25,32 +25,36 @@ export default function Post({ post }) {
     // console.log('comments', comments)
     return (
         <div id='Post' className='ordersCont'>
-            {post.this_user === user.username && <div className='commentBtnsCont'>
-            <button onClick={async () => {
-                await deletePost(post.id)
-                navigate(0)
-            }}>X Delete My Post</button>
-            <button onClick={async () => {
-                setShowEditForm(!showEditForm)
-            }}>+ Edit My Post</button>
-        </div>}
-        {!showEditForm ? <>
-            <h1>"{post.title}"</h1>
-            <h2>-{post.this_user}</h2>
-            <h3><i>{post.this_topic}</i></h3>
-            <h3>{moment(post.created_at).format('MM/DD/YYYY hh:mm a')}</h3>
-            <p>{post.body}</p>
-            <hr />
+
+            {!showEditForm ? <>
+                <h1>"{post.title}"</h1>
+                <h2>-{post.this_user}</h2>
+                <h3><i>{post.this_topic}</i></h3>
+                <h3>{moment(post.created_at).format('MM/DD/YYYY hh:mm a')}</h3>
+                <p>{post.body}</p>
+                <hr />
+            </> : <EditPostForm post={post} user={user} />}
             <div className='commentBtnsCont'>
-                <button onClick={() => setShowCommentForm(!showCommentForm)}>Add Comment</button>
-                {comments.length > 0 &&
-                    <button onClick={() => setShowComments(!showComments)}>{!showComments ? 'Show' : 'Hide'} Comments ({comments.length})</button>}
+                <div>
+                    <button onClick={() => setShowCommentForm(!showCommentForm)}>Add Comment</button>
+                    {comments.length > 0 &&
+                        <button onClick={() => setShowComments(!showComments)}>{!showComments ? 'Show' : 'Hide'} Comments ({comments.length})</button>}
+                </div>
+                {post.this_user === user.username && <div>
+                    <button onClick={async () => {
+                        await deletePost(post.id)
+                        navigate(0)
+                    }}>Delete My Post</button>
+                    <button onClick={() => {
+                        setShowEditForm(!showEditForm)
+                    }}>Edit My Post</button>
+                </div>}
             </div>
             {showCommentForm && <NewCommentForm postId={post.id} />}
             {showComments && [...comments].reverse().map((comment, i) => {
                 return <Comment comment={comment} key={i} />
             })}
-        </> : <EditPostForm post={post} user={user}/>}
+
         </div>
     )
 }
